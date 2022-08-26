97ee80536c72ec728be6db2314ef455155ae2ef9
Kaushik Das <kaushik762das@gmail.com>
2022-08-26 18:12:22 +0530

    Day9 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ import 'package:firebase_core/firebase_core.dart';
 void main()async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  
+  runApp(const MyApp());
 }
 
 
